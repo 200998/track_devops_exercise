5dd618426cebe6b225e8f7258a370bf93e7b95cd
200998 <toshiki_kondo_gst@jp.honda>
2025-03-19 20:34:43 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,26 @@
 def add(a, b, c=0):
+    if type(a) != int:
+        ret_a = -1
+    elif a<0 or 10<a:
+        ret_a = -2
+    else:
+        ret_a = a
+
+    if type(b) != int:
+        ret_b = -1
+    elif b<0 or 10<b:
+        ret_b = -2
+    else:
+        ret_b = b
+
+    if type(c) != int:
+        ret_c = -1
+    elif c<0 or 10<c:
+        ret_c = -2
+    else:
+        ret_c = c
+    
     try:
-        return int(a + b) + int(c)
+        return int(ret_a + ret_b) + int(ret_c)
     except:
         return "error"
